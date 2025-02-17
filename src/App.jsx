bb017094471dcd9bd46eee9cfe6ade1fff376e74
Johana Castellano <johanacg16@gmail.com>
2025-02-17 15:01:29 +0000

    .env added and test axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css'
 
+/* import.meta.env.VITE_BACK_URL */
+
 function App() {
+  const [gods, setGods] = useState([]);
+
+  const getAllGods = () => {
+    axios
+      .get(`${import.meta.env.VITE_BACK_URL}/gods`)
+      .then((response) => setGods(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllGods();
+  }, [] );
 
   return (
     <>
       <h1>MythLog</h1>
+
+      {gods.map((god) => {
+          return (
+            <div className="ProjectCard card" key={god.id} >
+              
+                <h3>{god.name}</h3>
+                <img className="card-image" src={god.image} alt="" />
+                <h4>{god.category}</h4>
+                <p>{god.description}</p>
+                <p>{god.attributes.stories}</p>
+            </div>
+          );
+        })}
       
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         
-      </p>
+      </p> */}
     </>
   )
 }
